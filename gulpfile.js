22a4eb8431ee32b42ae3762fd7aae8307e70eae9
Berk Cengiz <berkcengiiz@gmail.com>
2023-01-26 21:40:26 +0300

    fix: fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
 const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
 
-gulp.task('merhaba', function(){
+gulp.task('sass', function(){
     // console.log("çalıştı")
     return gulp.src('./scss/main.scss')
     .pipe(sass())
     .pipe(gulp.dest('./'))
 });
+
+gulp.task(['sass'])
