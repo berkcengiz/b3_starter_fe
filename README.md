237d7db8cbfc5cf6166aa3cf4e06e644881fe17e
Berk Cengiz <berkcengiiz@gmail.com>
2023-01-30 20:54:25 +0300

    fix: dev

--- README.md
+++ README.md
@@ -5,3 +5,13 @@ NPM Packet Uninstall:
 npm uninstall package-name
 
 //------------------------------
+//------------------------------
+
+Gulp Create New Task:
+
+1. where is my scss file
+2. pass that file through sass compiler
+3. where do I save the compiled css ?
+4. stream changes to all browser
+
+//------------------------------
--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,73 @@
-const gulp = require('gulp');
-const cleanCSS = require('gulp-clean-css');
-const concat = require('gulp-concat');
-const rename = require("gulp-rename");
-const sass = require('gulp-sass')(require('sass'));
-const uglify = require('gulp-uglify');
+const gulp          = require('gulp');
+const sass          = require('gulp-sass')(require('sass'));
+const concat        = require('gulp-concat');
+const uglify        = require('gulp-uglify');
+const cleanCSS      = require('gulp-clean-css');
+const rename        = require("gulp-rename");
+const sourcemaps    = require('gulp-sourcemaps');
+const { src, series, parallel, dest, watch } = require('gulp');
 
 
-gulp.task('styles', function(){
-    return gulp.src('./src/scss/**/**.scss')
-    .pipe(sass())
+// file paths
+const files = {
+    htmlPath:   './views/pages/**/*.html',
+    scssPath:   './src/scss/**/*.scss',
+    jsPath:     './src/javascript/**/*.js',
+    mediaPath:  './src/media/**/**',
+    fontPath:   './src/fonts/*',
+}
+
+
+// html
+function html() {
+    return gulp.src(files.htmlPath)
+    .pipe(gulp.dest('./dist'))
+}
+
+
+// scss
+function scss() {
+    return gulp.src(files.scssPath)
+    .pipe(sass().on('error', sass.logError))
     .pipe(cleanCSS())
     .pipe(rename({
         basename: "style",
         suffix : ".min"
     }))
-    .pipe(gulp.dest('./dist/src/css/'))
-});
+    .pipe(sourcemaps.init())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./dist/src/css'))
+}
 
-gulp.task('javascript', function(){
-    return gulp.src([
-        './src/javascript/**/**.js',
-        '!./src/javascript/library/*'
-    ])
+
+// js
+function js() {
+    return gulp.src(files.jsPath)
     .pipe(concat('core.min.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('./dist/src/js/'))
-});
+    .pipe(sourcemaps.init())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./dist/src/js'))
+}
 
-gulp.task('page', function(){
-    return gulp.src('./views/pages/**/**.html')
-    .pipe(gulp.dest('./dist/'))
-});
 
-gulp.task('media', function(){
-    return gulp.src('./src/media/**/**')
-    .pipe(gulp.dest('./dist/media'))
-});
+// media
+function media() {
+    return gulp.src(files.mediaPath)
+    .pipe(gulp.dest('./dist/src/media'))
+}
 
-gulp.task('font', function(){
-    return gulp.src('./src/fonts/**/**')
-    .pipe(gulp.dest('./dist/fonts'))
-});
 
-gulp.task(['styles', 'javascript', 'page', 'media', 'font'])
+// font
+function font() {
+    return gulp.src(files.fontPath)
+    .pipe(gulp.dest('./dist/src/fonts'))
+}
+
+
+exports.html    = html;
+exports.scss    = scss;
+exports.js      = js;
+exports.media   = media;
+exports.font    = font;
+exports.default = parallel(html, scss, js, media, font);
