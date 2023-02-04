13e11af3ff87be827d523451959c42be988f4c16
Berk Cengiz <berkcengiiz@gmail.com>
2023-02-04 16:21:48 +0300

    fix: dev

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -3,11 +3,7 @@
     Author URI: https://berkcengiz.com
 */
 
-console.log(
-    "%cb3 house | Creative Digital Production 😎",
-    "background:#FFC000; color:#151515; padding:3px 6px;"
-);
-
 function App() {}
 
 App();
+b3log();
---
+++ src/javascript/library/b3-log.js
@@ -0,0 +1,6 @@
+function b3log() {
+    console.log(
+        "%cb3 house | Creative Digital Production 😎",
+        "background:#FFC000; color:#151515; padding:3px 6px;"
+    );
+}
