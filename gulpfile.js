66d90f4c068941bd3abcb1bde440db415cd417cd
Berk Cengiz <berkcengiiz@gmail.com>
2023-10-27 10:47:39 +0300

    fix: dev

--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,6 @@ const files = {
         "node_modules/bootstrap/dist/js/bootstrap.bundle.min.js",
         "node_modules/vanilla-lazyload/dist/lazyload.min.js",
         "node_modules/swiper/swiper-bundle.min.js",
-        "node_modules/select2/dist/js/select2.min.js",
     ],
 };
 
