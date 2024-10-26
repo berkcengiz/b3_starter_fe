2aafa114db9f0c1135e1c3bffb24fc8f1db9fb74
Berk Cengiz <berkcengiiz@gmail.com>
2024-10-27 01:11:02 +0300

    fix: dev

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,15 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const concat = require("gulp-concat");
-const uglify = require("gulp-uglify");
+const uglify = require("gulp-uglify-es").default; // gulp-uglify-es kullanımı
 const cleanCSS = require("gulp-clean-css");
 const rename = require("gulp-rename");
 const sourcemaps = require("gulp-sourcemaps");
 const nunjucksRender = require("gulp-nunjucks-render");
 const browserSync = require("browser-sync").create();
-const { src, series, parallel, dest, watch } = require("gulp");
+const { series, parallel } = require("gulp");
 
-// file paths
+// File paths
 const files = {
     htmlPath: "./views/pages/**/*.+(html|nunjucks)",
     htmlWatchPath: "./views/**/**/*.+(html|nunjucks)",
@@ -18,7 +18,7 @@ const files = {
     mediaPath: "./src/media/**/**",
     fontPath: "./src/fonts/*",
 
-    // library file paths
+    // Library file paths
     jsLibPath: [
         "node_modules/jquery/dist/jquery.min.js",
         "node_modules/bootstrap/dist/js/bootstrap.bundle.min.js",
@@ -27,62 +27,53 @@ const files = {
     ],
 };
 
-// html
+// HTML Task
 function html() {
     return gulp
         .src(files.htmlPath)
-        .pipe(
-            nunjucksRender({
-                path: ["views"],
-            })
-        )
+        .pipe(nunjucksRender({ path: ["views"] }))
         .pipe(gulp.dest("./dist"))
         .pipe(browserSync.stream());
 }
 
-// scss
+// SCSS Task
 function scss() {
     return gulp
         .src(files.scssPath)
+        .pipe(sourcemaps.init())
         .pipe(sass().on("error", sass.logError))
         .pipe(cleanCSS())
-        .pipe(
-            rename({
-                basename: "style",
-                suffix: ".min",
-            })
-        )
-        .pipe(sourcemaps.init())
+        .pipe(rename({ basename: "style", suffix: ".min" }))
         .pipe(sourcemaps.write("."))
         .pipe(gulp.dest("./dist/src/css"))
         .pipe(browserSync.stream());
 }
 
-// jsCore
+// JS Core Task
 function jsCore() {
     return gulp
         .src(files.jsCorePath)
+        .pipe(sourcemaps.init())
         .pipe(concat("core.min.js"))
         .pipe(uglify())
-        .pipe(sourcemaps.init())
         .pipe(sourcemaps.write("."))
         .pipe(gulp.dest("./dist/src/js"))
         .pipe(browserSync.stream());
 }
 
-// jsLib
+// JS Library Task
 function jsLib() {
     return gulp
         .src(files.jsLibPath)
+        .pipe(sourcemaps.init())
         .pipe(concat("lib.min.js"))
         .pipe(uglify())
-        .pipe(sourcemaps.init())
         .pipe(sourcemaps.write("."))
         .pipe(gulp.dest("./dist/src/js"))
         .pipe(browserSync.stream());
 }
 
-// media
+// Media Task
 function media() {
     return gulp
         .src(files.mediaPath)
@@ -90,7 +81,7 @@ function media() {
         .pipe(browserSync.stream());
 }
 
-// font
+// Font Task
 function font() {
     return gulp
         .src(files.fontPath)
@@ -98,20 +89,18 @@ function font() {
         .pipe(browserSync.stream());
 }
 
+// Watch Task
 function watchTask() {
-    browserSync.init({
-        server: {
-            baseDir: "./dist/",
-        },
-    });
+    browserSync.init({ server: { baseDir: "./dist/" } });
     gulp.watch(files.htmlWatchPath, html).on("change", browserSync.reload);
-    gulp.watch(files.scssPath, scss).on("change", browserSync.reload);
-    gulp.watch(files.jsCorePath, jsCore).on("change", browserSync.reload);
-    gulp.watch(files.jsLibPath, jsLib).on("change", browserSync.reload);
-    gulp.watch(files.fontPath, font).on("change", browserSync.reload);
-    gulp.watch(files.mediaPath, media).on("change", browserSync.reload);
+    gulp.watch(files.scssPath, scss);
+    gulp.watch(files.jsCorePath, jsCore);
+    gulp.watch(files.jsLibPath, jsLib);
+    gulp.watch(files.fontPath, font);
+    gulp.watch(files.mediaPath, media);
 }
 
+// Default Task
 exports.html = html;
 exports.scss = scss;
 exports.jsCore = jsCore;
