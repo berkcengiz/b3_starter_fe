18a855f7e6ec8cd796a0956226e8cc2cca1cd062
Berk Cengiz <berkcengiiz@gmail.com>
2024-11-14 00:22:07 +0300

    fix: dev

---
+++ src/javascript/library/slow.js
@@ -0,0 +1,17 @@
+// function Slow() {
+//     document.body.style.display = "none";
+
+//     function delayPageLoad(ms) {
+//         const end = Date.now() + ms;
+//         while (Date.now() < end) {
+//             Math.random();
+//         }
+//     }
+
+//     window.addEventListener("DOMContentLoaded", function () {
+//         delayPageLoad(15000);
+//         document.body.style.display = "block";
+//     });
+// }
+
+// Slow();
